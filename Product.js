57bf52a77323610497b3c87dc1dcb817927aa4e5
dani-sotano <daniela.keller1992@gmail.com>
2022-07-06 20:24:55 -0700

    cleanup

---
+++ Product.js
@@ -0,0 +1,3 @@
+const Product = () => {
+    return ( `<div id="product">This is the product component</div>` )
+} 
---
+++ Service.js
@@ -0,0 +1,3 @@
+const Service = () => {
+    return ( `<div id="service">This is the service component</div>` )
+} 
